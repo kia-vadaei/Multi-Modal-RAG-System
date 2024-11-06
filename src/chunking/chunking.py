032481cae09eb48f375b77d2957720d7d937573f
Kianoosh Vadaei <kia.vadaei@gmail.com>
2024-11-07 00:24:34 +0330

    Saliency was added

--- src/chunking/chunking.py
+++ src/chunking/chunking.py
@@ -105,3 +105,73 @@ class ClipChunking(Chunking):
     def evaluate(self):
         self.avg_frame_per_chunk = super().get_avg_frame_per_time()
         self.mean_entropy = super().get_mean_entropy()
+
+
+class SaliencyChunking(Chunking):
+    def __init__(self):
+        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.chunks = None
+        self.exe_time = None
+
+    def spectral_residual(self, frame):
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        f_transform = np.fft.fft2(gray_frame)
+        f_transform_shifted = np.fft.fftshift(f_transform)
+
+        magnitude = np.abs(f_transform_shifted)
+        log_magnitude = np.log1p(magnitude)  # log(1 + x) to avoid log(0)
+        spectral_residual = np.exp(log_magnitude - np.mean(log_magnitude))
+
+        return spectral_residual
+    
+    def detect_slide_changes(self, video_path, threshold=0.125, interval=1):
+        cap = cv2.VideoCapture(video_path)
+        success, frame = cap.read()
+
+        frame_lst = []
+        Saliency_chunks = []
+
+        if not success:
+            print("Error: Failed to read the first frame.")
+            return []
+
+        prev_spectral_residual = self.spectral_residual(frame)
+        timestamp = 0
+        timestamps = []
+
+        while cap.isOpened():
+            # Move to the next frame based on the interval
+            cap.set(cv2.CAP_PROP_POS_MSEC, timestamp * 1000)
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            spectral_residual_frame = self.spectral_residual(frame)
+            mean_residual = np.mean(spectral_residual_frame)
+
+            if abs(mean_residual - np.mean(prev_spectral_residual)) > threshold:
+                minutes = int(timestamp // 60)
+                seconds = int(timestamp % 60)
+                print(f"Slide changed at {minutes:02}:{seconds:02} minutes")  # Print slide change time
+                timestamps.append(timestamp)
+                Saliency_chunks.append(frame_lst)
+                frame_lst = []
+
+            prev_spectral_residual = spectral_residual_frame
+
+            frame_lst.append(frame)
+
+            timestamp += interval
+
+        cap.release()
+        return Saliency_chunks, timestamps
+
+
+    
+    def chunk(self, video_path):
+        start_time = time()
+        self.chunks, slide_change_timestamps = self.detect_slide_changes(video_path, threshold=0.8, interval=1)
+        end_time = time()
+        self.exe_time = end_time - start_time
+        return self.chunks, slide_change_timestamps, self.exe_time
